6774b2bf09705ea9e271796c3c30868c1e534871
panos-charalampopoulos <panos10cha@gmail.com>
2022-04-17 23:06:27 +0300

    no functionallity

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-function Book(title, author, pages, read) {
+
+
+
+let myLibrary = [
+    { book: "harry potter" },
+    { book: "spider man" }
+];
+
+function Book(title, author, pages,) {
     this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
-    this.info = function() {
-        return this.title + " " + "by" + " " + this.author + "," + " " + this.pages + " " + "pages" + "," + " " + this.read;
+        this.author = author,
+        this.pages = pages
+    this.info = function () {
+        return this.title + " " + "by" + " " + this.author + "," + " " + this.pages + " " + "pages";
     }
 };
-var book = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet" )
+
+
+
+function addBookToLibrary() {
+    let titleInput = document.querySelector(".title").value
+    let authorInput = document.querySelector(".author").value
+    let pagesInput = document.querySelector(".pages").value
+
+    let book1 = new Book("titleInput", "authorInput", "pagesInput");
+    myLibrary.push(book1);
+};
+
+function render() {
+    for (var i = 0; i < myLibrary.length; i++) {
+        document.getElementsByTagName("input") .innerHTML += "<table>" + myLibrary[i].book + "</table>";
+    };
+
+};
+
+
+
+
+
+
+
+
