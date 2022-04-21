b051ec3da6a94b46265f629867b3ad50da4dfadc
panos-charalampopoulos <panos10cha@gmail.com>
2022-04-21 18:10:05 +0300

    incomplete

--- main.js
+++ main.js
@@ -1,42 +1,61 @@
-
-
-
-let myLibrary = [
-    { book: "harry potter" },
-    { book: "spider man" }
-];
-
-function Book(title, author, pages,) {
-    this.title = title,
-        this.author = author,
-        this.pages = pages
-    this.info = function () {
-        return this.title + " " + "by" + " " + this.author + "," + " " + this.pages + " " + "pages";
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
     }
 };
 
+const title = document.getElementById("title").value;
+const author = document.getElementById("author").value;
+const pages = document.getElementById("pages").value;
+const button = document.getElementById("btn").value;
 
+let myLibrary = [];
 
 function addBookToLibrary() {
-    let titleInput = document.querySelector(".title").value
-    let authorInput = document.querySelector(".author").value
-    let pagesInput = document.querySelector(".pages").value
 
-    let book1 = new Book("titleInput", "authorInput", "pagesInput");
-    myLibrary.push(book1);
-};
+    let book1 = new Book(title, author, pages);
+
+    myLibrary.push(book1)
+
+    let book2 = new Book(title, author, pages);
+
+    myLibrary.push(book2)
+
+}
+
+
+
+button.addEventListener("click", function addBookToLibrary() {
+    if (title.value == "" &&
+        author.value == "" &&
+        pages.value == "") {
+        alert("Enter input");
+    } else {
+        const bookListRow = document.createElement("tr")
+
+        const newTitle = document.createElement("th");
+        newTitle.innerHTML = title.value;
+        bookListRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement("th");
+        newAuthor.innerHTML = Author.value;
+        bookListRow.appendChild(newAuthor);
+
+        const newPages = document.createElement("th");
+        newPages.innerHTML = pages.value;
+        bookListRow.appendChild(newPages);
+    }
+});
+
+
+
+
 
 function render() {
     for (var i = 0; i < myLibrary.length; i++) {
-        document.getElementsByTagName("input") .innerHTML += "<table>" + myLibrary[i].book + "</table>";
+        document.getElementsByTagName("input").innerHTML += "<table>" + myLibrary[i].book + "</table>";
     };
 
 };
-
-
-
-
-
-
-
-
